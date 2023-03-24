82d2d29e32f0960cc29214cbfcc3c43d9f957be0
Angel Rodriguez <53921920+angelz510@users.noreply.github.com>
2023-03-24 08:46:14 -0700

    update post with likes

--- controllers/posts.js
+++ controllers/posts.js
@@ -46,3 +46,30 @@ export const getUserPosts = async (req, res) => {
     res.status(404).json({ message: err.message });
   }
 };
+
+// UPDATE POST
+export const likePost = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { userId } = req.body;
+    const post = await post.findById(id);
+    const isLiked = post.likes.get(userId);
+
+    if (isLiked) {
+      post.likes.delete(userId);
+    } else {
+      post.likes.set(userId, true);
+    }
+
+    const updatedPost = await Post.findByIdAndUpdate(
+      id,
+      { likes: post.likes },
+      { new: true }
+    );
+    res.status(200).json(updatedPost);
+
+    res.status(200).json();
+  } catch (err) {
+    res.status(404).json({ message: err.message });
+  }
+};
